289ce58fca02b53b9d7c8b64fd5b07edbed3577f
Ricardo Neves <ricardo@ricardoneves.me>
2022-01-24 11:36:03 -0300

    criação da app Empresas

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # APPS da aplicação criada
+    'empresas',
 ]
 
 MIDDLEWARE = [
